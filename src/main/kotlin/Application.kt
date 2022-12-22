2b40a29d2a1f21cde0bcc07975abf1f577920646
JorgePBrown <jorgepbrown@gmail.com>
2022-12-22 12:08:53 +0000

    Added shutdownHook to kill mongo connection

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,17 +1,67 @@
+import api.configureRouting
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.routing.*
 import io.ktor.server.application.*
+import io.ktor.server.config.*
+import io.ktor.server.engine.*
 import io.ktor.server.html.*
+import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
-import kotlinx.coroutines.async
+import io.ktor.server.routing.*
 import kotlinx.html.*
-import repo.day.DayMapper
-import repo.day.DayRepo
 
-fun main(args: Array<String>): Unit =
-    io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>) {
+    val applicationEnvironment = commandLineEnvironment(args)
+    val engine = NettyApplicationEngine(applicationEnvironment) { loadConfiguration(applicationEnvironment.config) }
+
+    val gracePeriod =
+        engine.environment.config.propertyOrNull("ktor.deployment.shutdownGracePeriod")?.getString()?.toLong()
+            ?: 3000
+    val timeout =
+        engine.environment.config.propertyOrNull("ktor.deployment.shutdownTimeout")?.getString()?.toLong()
+            ?: 5000
+    engine.addShutdownHook {
+        engine.stop(gracePeriod, timeout)
+    }
+    engine.addShutdownHook {
+        MongoConnection.close()
+    }
+
+    engine.start(true)
+}
+
+internal fun NettyApplicationEngine.Configuration.loadConfiguration(config: ApplicationConfig) {
+    val deploymentConfig = config.config("ktor.deployment")
+    loadCommonConfiguration(deploymentConfig)
+    deploymentConfig.propertyOrNull("requestQueueLimit")?.getString()?.toInt()?.let {
+        requestQueueLimit = it
+    }
+    deploymentConfig.propertyOrNull("runningLimit")?.getString()?.toInt()?.let {
+        runningLimit = it
+    }
+    deploymentConfig.propertyOrNull("shareWorkGroup")?.getString()?.toBoolean()?.let {
+        shareWorkGroup = it
+    }
+    deploymentConfig.propertyOrNull("responseWriteTimeoutSeconds")?.getString()?.toInt()?.let {
+        responseWriteTimeoutSeconds = it
+    }
+    deploymentConfig.propertyOrNull("requestReadTimeoutSeconds")?.getString()?.toInt()?.let {
+        requestReadTimeoutSeconds = it
+    }
+    deploymentConfig.propertyOrNull("tcpKeepAlive")?.getString()?.toBoolean()?.let {
+        tcpKeepAlive = it
+    }
+    deploymentConfig.propertyOrNull("maxInitialLineLength")?.getString()?.toInt()?.let {
+        maxInitialLineLength = it
+    }
+    deploymentConfig.propertyOrNull("maxHeaderSize")?.getString()?.toInt()?.let {
+        maxHeaderSize = it
+    }
+    deploymentConfig.propertyOrNull("maxChunkSize")?.getString()?.toInt()?.let {
+        maxChunkSize = it
+    }
+}
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
@@ -19,22 +69,5 @@ fun Application.module() {
         json()
     }
 
-    routing {
-        get("/") {
-            call.respondHtml {
-                head {
-                    title {
-                        +"Mood Tracker"
-                    }
-                }
-                body {
-                    h1 {
-                        +"Hello World!"
-                    }
-                }
-            }
-        }
-
-        dayRoute(DayRepo(DayMapper()))
-    }
+    configureRouting()
 }
